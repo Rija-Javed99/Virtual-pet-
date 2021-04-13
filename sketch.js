071454b691da2dceaddda5ae83b21bd1192070e9
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-13 14:50:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,21 +14,23 @@ function setup() {
   createCanvas(1000,400);
 
   foodObj = new Food();
-
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
+ //take ref for foodstock
   
-  dog=createSprite(800,200,150,150);
-  dog.addImage(sadDog);
-  dog.scale=0.15;
   
-  feed=createButton("Feed the dog");
-  feed.position(700,95);
-  feed.mousePressed(feedDog);
-
-  addFood=createButton("Add Food");
-  addFood.position(800,95);
-  addFood.mousePressed(addFoods);
+  
+  
+  
+  //create dog
+  
+  
+  
+  
+  //create buttons for "feed" and "addFood" and set mousePressed function
+  
+  
+  
+  
+  
 
 }
 
@@ -36,13 +38,14 @@ function draw() {
   background(46,139,87);
   foodObj.display();
 
-  fedTime=database.ref('FeedTime');
-  fedTime.on("value",function(data){
-    lastFed=data.val();
-  });
+  //set fedTime ref.
+  
+  
+  
  
   fill(255,255,254);
   textSize(15);
+  
   if(lastFed>=12){
     text("Last Feed : "+ lastFed%12 + " PM", 350,30);
    }else if(lastFed==0){
@@ -54,7 +57,6 @@ function draw() {
   drawSprites();
 }
 
-//function to read food Stock
 function readStock(data){
   foodS=data.val();
   foodObj.updateFoodStock(foodS);
@@ -78,10 +80,11 @@ function feedDog(){
   })
 }
 
-//function to add food in stock
+//function to add food in stock on each click
 function addFoods(){
-  foodS++;
-  database.ref('/').update({
-    Food:foodS
-  })
+
+  
+  
+  
+  
 }
